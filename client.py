88cfccd8e43240fe61979e1f4d19d0d08ff49928
Nishanth <nishanthdmello2003@gmail.com>
2023-03-28 19:32:55 +0530

    working model of attendance report

--- client.py
+++ client.py
@@ -1,22 +1,30 @@
 import socket
+import pickle
 
 # Create a socket object
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Get local machine name and port number
-host = socket.gethostname()
+host = "127.0.0.1"
 port = 9999
 
 # Connect to the server
 client_socket.connect((host, port))
 
+# Receive the srns
+data = client_socket.recv(1024)
+data = pickle.loads(data)
+
 # Prompt the user for attendance data
-student_id = input("Enter student ID : ")
-attendance_status = input("Enter attendance status (P or A) : ")
+attendance_status = []
+for i in data:
+    print("Is",i, end=" ")
+    attendance = input("(P or A) : ")
+    attendance_status.append(attendance)
 
 # Send the attendance data to the server
-data = student_id + "," + attendance_status
-client_socket.sendall(data.encode())
+data = pickle.dumps(attendance_status)
+client_socket.send(data)
 
 # Receive the server's response
 response = client_socket.recv(1024)
--- server.py
+++ server.py
@@ -1,10 +1,19 @@
 import socket
+import csv
+import pickle
+
+# Get srns from file
+csv_file = open("Data.csv", "r")
+csv_reader = csv.reader(csv_file)
+data = next(csv_reader)
+csv_file.close()
+data = pickle.dumps(data)
 
 # Create a socket object
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Get local machine name and port number
-host = socket.gethostname()
+host = "127.0.0.1"
 port = 9999
 
 # Bind socket to host and port
@@ -13,28 +22,27 @@ server_socket.bind((host, port))
 # Listen for incoming client connections
 server_socket.listen(5)
 
-# Create an empty dictionary to store attendance data
-attendance_data = {}
-
 while True:
+
     # Wait for a client to connect
     client_socket, addr = server_socket.accept()
     print("Connection from : " + str(addr))
 
+    # Send srns of students
+    client_socket.send(data)
+
     # Receive attendance data from the client
-    data = client_socket.recv(1024)
+    attendance_data = client_socket.recv(1024)
+    attendance_data = pickle.loads(attendance_data)
 
-    # Check if the data is valid
-    if data:
-        # Parse the attendance data and store it in the dictionary
-        student_id, attendance_status = data.decode().split(",")
-        attendance_data[student_id] = attendance_status
+    # Write to csv file
+    csv_file = open("Data.csv", "a")
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(attendance_data)
+    csv_file.close()
 
-        # Send a success response back to the client
-        client_socket.send(b"Attendance successfully recorded.")
-    else:
-        # Send an error response back to the client
-        client_socket.send(b"Invalid attendance data.")
+    # Send a success response back to the client
+    client_socket.send(b"Attendance successfully recorded.")
 
     # Close the client connection
     client_socket.close()

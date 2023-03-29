e4edccedc0bf8f6fa966b1034fd0305ffd6d6535
Nishanth <nishanthdmello2003@gmail.com>
2023-03-29 20:28:57 +0530

    Added GUI using tkinter

--- client.py
+++ client.py
@@ -1,36 +1,79 @@
 import socket
 import pickle
+from tkinter import *
 
-# Create a socket object
+
+def send(list_of_absentees):
+
+    # Send the attendance data to the server
+    data = pickle.dumps(list_of_absentees)
+    client_socket.send(data)
+
+    # Receive the server's response
+    response = client_socket.recv(1024)
+
+    # Print the response
+    print(response.decode())
+
+    # Close the socket
+    client_socket.close()
+
+
+def submit():
+
+    list_of_absentees = ["p", "p", "p"]
+    for i in range(3):
+        if checkbox_variables[i].get() == 1:
+            list_of_absentees[i] = "a"
+    send(list_of_absentees)
+    root.destroy()
+
+
+# Socket object created
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Get local machine name and port number
+# Getting local machine name and port number
 host = "127.0.0.1"
 port = 9999
 
-# Connect to the server
+# Connecting to the server
 client_socket.connect((host, port))
 
-# Receive the srns
+# Receiving the srns and unpickling it
 data = client_socket.recv(1024)
 data = pickle.loads(data)
 
-# Prompt the user for attendance data
-attendance_status = []
-for i in data:
-    print("Is",i, end=" ")
-    attendance = input("(P or A) : ")
-    attendance_status.append(attendance)
 
-# Send the attendance data to the server
-data = pickle.dumps(attendance_status)
-client_socket.send(data)
+root = Tk()
+root.title("Attendance")
+root.geometry("200x200")
 
-# Receive the server's response
-response = client_socket.recv(1024)
+l1 = Label(root, text="SRN")
+l2 = Label(root, text="Absentees")
+l1.grid(row=0, column=0)
+l2.grid(row=0, column=1)
 
-# Print the response
-print(response.decode())
+dn = IntVar()
+na = IntVar()
+nd = IntVar()
 
-# Close the socket
-client_socket.close()
+checkbox_variables = [dn, na, nd]
+
+cb1 = Checkbutton(root, variable=checkbox_variables[0], onvalue=1, offvalue=0)
+cb2 = Checkbutton(root, variable=checkbox_variables[1], onvalue=1, offvalue=0)
+cb3 = Checkbutton(root, variable=checkbox_variables[2], onvalue=1, offvalue=0)
+cb1.grid(row=1, column=1)
+cb2.grid(row=2, column=1)
+cb3.grid(row=3, column=1)
+
+srn1 = Label(root, text=data[0])
+srn2 = Label(root, text=data[1])
+srn3 = Label(root, text=data[2])
+srn1.grid(row=1, column=0)
+srn2.grid(row=2, column=0)
+srn3.grid(row=3, column=0)
+
+but = Button(root, text="Submit", command=submit, font=("Helvetica", 12))
+but.grid(row=4, rowspan=3, column=0, columnspan=2,pady=20)
+
+mainloop()
